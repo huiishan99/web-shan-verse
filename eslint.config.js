5b3863384e16e99b60a65f948286b743862acb1c
Hui Shan <huiishan99@gmail.com>
2026-05-31 00:24:13 +0900

    Add quality gates

---
+++ eslint.config.js
@@ -0,0 +1,40 @@
+import js from '@eslint/js';
+import eslintPluginAstro from 'eslint-plugin-astro';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+
+export default [
+  {
+    ignores: [
+      '.astro/**',
+      'dist/**',
+      'node_modules/**',
+    ],
+  },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...eslintPluginAstro.configs.recommended,
+  {
+    files: ['**/*.{js,mjs,ts}'],
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+    },
+  },
+  {
+    files: ['src/**/*.astro', 'src/**/*.astro/*.js', 'src/**/*.astro/*.ts'],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+  },
+  {
+    files: ['src/**/*.astro/*.js', 'src/**/*.astro/*.ts'],
+    rules: {
+      'no-undef': 'off',
+    },
+  },
+];
